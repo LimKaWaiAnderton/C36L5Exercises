aac49ca7f5f94aa1c9611911729fd0756f7a8c61
Anderton <23054103@myrp.edu.sg>
2024-11-15 17:39:09 +0800

    Changes

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SectionList } from 'react-native';
 
-export default function App() {
+// 
+// ==============================================================================================
+// Exercise 1
+
+//const datasource = [
+//   { key: 'a' }, { key: 'b' }, { key: 'c' }, { key: 'd' }, { key: 'e' }, { key: 'f' }, 
+//   { key: 'g' }, { key: 'h' }, { key: 'i' }, { key: 'j' }, { key: 'k' }, { key: 'l' }, 
+//   { key: 'm' }, { key: 'n' }, { key: 'o' }, { key: 'p' }, { key: 'q' }, { key: 'r' }, 
+//   { key: 's' }, { key: 't' }, { key: 'u' }, { key: 'v' }, { key: 'w' }, { key: 'x' }, 
+//   { key: 'y' }, { key: 'z' }
+// ];
+// const RenderItem = ({ item }) => {
+//   return (
+//     <View style={styles.opacityStyle}>
+//       <Text style={styles.textStyle}>{item.key}</Text>
+//     </View>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <View style={{ marginTop: 50 }}>
+//       <FlatList data={datasource} renderItem={RenderItem} />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   opacityStyle: {
+//     borderWidth: 1,
+//   },
+
+//   textStyle: {
+//     margin: 10,
+//     fontSize: 15,
+//     textAlign: 'left'
+//   },
+// });
+// ==============================================================================================
+// Exercise 2
+
+const datasource = [
+  {
+    data: [
+      { key: 'a' }, { key: 'e' }, { key: 'i' }, { key: 'o' }, { key: 'u' }
+    ],
+    title: 'Vowels', bgcolor: 'skyblue'
+  },
+  {
+    data: [
+      { key: 'b' }, { key: 'c' }, { key: 'd' }, { key: 'f' }, { key: 'g' },
+      { key: 'h' }, { key: 'j' }, { key: 'k' }, { key: 'l' }, { key: 'm' },
+      { key: 'n' }, { key: 'p' }, { key: 'q' }, { key: 'r' }, { key: 's' },
+      { key: 't' }, { key: 'v' }, { key: 'w' }, { key: 'x' }, { key: 'y' },
+      { key: 'z' }
+    ],
+    title: 'Consonants', bgcolor: 'khaki'
+  }
+];
+
+const RenderItem = ({ item }) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.opacityStyle}>
+      <Text style={styles.textStyle}>{item.key}</Text>
+    </View>
+  );
+}
+
+function App() {
+  return (
+    <View style={{ marginTop: 50 }}>
+      <SectionList sections={datasource} renderItem={RenderItem}
+        renderSectionHeader={({ section: { title, bgcolor } }) => (
+          <Text style={[styles.headerText, {backgroundColor: bgcolor}]}>{title}</Text>
+        )} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  opacityStyle: {
+    borderWidth: 1,
   },
+
+  textStyle: {
+    margin: 10,
+    fontSize: 15,
+    textAlign: 'left'
+  },
+
+  headerText: {
+    fontSize: 20,
+    margin: 10,
+    textAlign: 'center',
+    fontWeight: 'bold'
+  }
 });
+export default App;
